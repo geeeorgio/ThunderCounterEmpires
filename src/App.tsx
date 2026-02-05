a0d31d28699a1de0aa1cd2408f3ecced13824daa
george <heorhii.kyryliuk@gmail.com>
2026-02-05 20:03:12 +0200

    init, add packages, config dev deps & files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>Thunder Counter Empires</Text>
+    </View>
+  );
+};
+
+export default App;
