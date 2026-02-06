7c8cb1c8530d4a8c2b217e65c93c1f71ed39d1ed
george <heorhii.kyryliuk@gmail.com>
2026-02-06 20:36:33 +0200

    settings screen

--- src/screens/SettingsScreen/SettingsScreen.tsx
+++ src/screens/SettingsScreen/SettingsScreen.tsx
@@ -1,10 +1,86 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Switch, View } from 'react-native';
+
+import { styles } from './styles';
+
+import {
+  CustomButton,
+  CustomContainer,
+  CustomHeader,
+  CustomScreenWrapper,
+  CustomText,
+  ShareIcon,
+} from 'src/components';
+import { COLORS } from 'src/constants';
+import { handleShare, hp, wp } from 'src/utils';
 
 const SettingsScreen = () => {
+  const [vibration, setVibration] = useState(true);
+  const [notification, setNotification] = useState(false);
+
+  const handleSharePress = () => {
+    handleShare();
+  };
+
+  const handleVibrationChange = () => {
+    setVibration(!vibration);
+  };
+
+  const handleNotificationChange = () => {
+    setNotification(!notification);
+  };
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
-    </View>
+    <CustomScreenWrapper extraStyle={styles.container}>
+      <CustomHeader />
+      <CustomContainer
+        variant="purple_main"
+        extraStyle={styles.contentContainer}
+      >
+        <View style={styles.settingsContainer}>
+          <View style={styles.settingsItem}>
+            <CustomText extraStyle={styles.settingsItemText}>
+              Vibration
+            </CustomText>
+            <Switch
+              style={styles.switch}
+              value={vibration}
+              onValueChange={handleVibrationChange}
+              thumbColor={COLORS.white}
+              trackColor={{
+                true: COLORS.green_1,
+                false: COLORS.grey_border_55,
+              }}
+            />
+          </View>
+          <View style={styles.settingsItemSeparator} />
+          <View style={styles.settingsItem}>
+            <CustomText extraStyle={styles.settingsItemText}>
+              Notification
+            </CustomText>
+            <Switch
+              style={styles.switch}
+              value={notification}
+              onValueChange={handleNotificationChange}
+              thumbColor={COLORS.white}
+              trackColor={{
+                true: COLORS.green_1,
+                false: COLORS.grey_border_55,
+              }}
+            />
+          </View>
+        </View>
+
+        <CustomButton
+          variant="purple_main"
+          onPress={handleSharePress}
+          extraStyle={styles.button}
+        >
+          <CustomText extraStyle={styles.buttonText}>Share the app</CustomText>
+          <ShareIcon width={wp(22)} height={hp(22)} />
+        </CustomButton>
+      </CustomContainer>
+    </CustomScreenWrapper>
   );
 };
 
--- src/screens/SettingsScreen/styles.ts
+++ src/screens/SettingsScreen/styles.ts
@@ -1,3 +1,53 @@
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({});
+import { COLORS, FONTS } from 'src/constants';
+import { hp, sp, wp } from 'src/utils';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    width: '100%',
+    paddingTop: hp(20),
+    paddingHorizontal: wp(20),
+    paddingBottom: hp(60),
+    justifyContent: 'space-between',
+  },
+  settingsContainer: {
+    width: '100%',
+    gap: hp(16),
+    paddingTop: hp(10),
+    paddingHorizontal: wp(16),
+  },
+  settingsItem: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  settingsItemSeparator: {
+    width: '100%',
+    height: hp(1),
+    backgroundColor: COLORS.grey_opacity_80,
+  },
+  settingsItemText: {
+    fontSize: sp(18),
+    fontFamily: FONTS.MontRegular,
+  },
+  switch: {
+    transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }],
+  },
+  button: {
+    width: '100%',
+    flexDirection: 'row',
+    gap: wp(10),
+    paddingHorizontal: wp(16),
+    paddingVertical: hp(12),
+  },
+  buttonText: {
+    fontSize: sp(20),
+    fontFamily: FONTS.IntBlack,
+  },
+});
